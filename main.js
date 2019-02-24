5664c378e9e61441d3b6537022e058fe7156e854
Haixiang <haixiang6123@gmail.com>
2019-02-24 14:58:57 -0800

    Add hashschange listener

--- main.js
+++ main.js
@@ -1,12 +1,30 @@
 // Get number
-const number = window.location.hash.substring(1)
+let number = window.location.hash.substring(1)
 
 // Get corresponding div
-const div = document.querySelector(`#div${number}`)
+let div = document.querySelector(`#div${number}`)
 div.style.display = 'block'
 
 // Get app
-const app = document.querySelector('#app')
+let app = document.querySelector('#app')
 
 // Show this div
 app.appendChild(div)
+
+window.addEventListener('hashchange', (event) => {
+    // Get number
+    number = window.location.hash.substring(1)
+
+    // Get corresponding div
+    div = document.querySelector(`#div${number}`)
+    div.style.display = 'block'
+
+    // Get app
+    app = document.querySelector('#app')
+
+    app.children[0].style.display = 'none'
+    document.body.appendChild(app.children[0])
+
+    // Show this div
+    app.appendChild(div)
+})
