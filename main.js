1d0dfa38050de96cc8319fa226c086b6d3b6b722
Haixiang <haixiang6123@gmail.com>
2019-02-24 21:43:57 -0800

    Use history mode

--- main.js
+++ main.js
@@ -1,5 +1,18 @@
 const div1 = document.createElement('div')
 div1.innerHTML = '1'
+const view1 = document.createElement('div')
+view1.innerHTML = 'View'
+view1.style.height = '100px'
+view1.style.width = '100px'
+view1.style.background = 'red'
+div1.appendChild(view1)
+const nested1 = document.createElement('div')
+nested1.innerHTML = '1.1'
+const nested2 = document.createElement('div')
+nested1.innerHTML = '1.2'
+const nested3 = document.createElement('div')
+nested1.innerHTML = '1.3'
+
 const div2 = document.createElement('div')
 div2.innerHTML = '2'
 const div3 = document.createElement('div')
@@ -9,18 +22,19 @@ div4.innerHTML = '4'
 const div404 = document.createElement('div')
 div404.innerHTML = '404'
 
+const app = document.querySelector('#app')
+
 const routeTable = {
-    '1': div1,
-    '2': div2,
-    '3': div3,
-    '4': div4
+    '/1': div1,
+    '/2': div2,
+    '/3': div3,
+    '/4': div4
 }
 
-function routing() {
+function routing(root) {
     // Get number
-    let number = window.location.hash.substring(1)
-    // Get app
-    let app = document.querySelector('#app')
+    // let number = window.location.hash.substring(1)
+    let number = window.location.pathname
 
     number = number || 1
 
@@ -28,13 +42,28 @@ function routing() {
     let div = routeTable[number] || div404
 
     // Show this div
-    app.innerHTML = ''
+    root.innerHTML = ''
     // Show this div
-    app.appendChild(div)
+    root.appendChild(div)
 }
 
-routing()
+function onStateChange() {
+    routing(app)
+}
 
-window.addEventListener('hashchange', (event) => {
-    routing()
-})
+routing(app)
+
+const allA = document.querySelectorAll('a.link')
+for (let a of allA) {
+    a.addEventListener('click', (event) => {
+        event.preventDefault()
+
+        window.history.pushState(null, '', a.getAttribute('href'))
+
+        onStateChange()
+    })
+}
+
+// window.addEventListener('hashchange', (event) => {
+//     routing(app)
+// })
